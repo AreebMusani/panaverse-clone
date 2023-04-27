dbcd6ed1f1419962d0347d3acbe59511acf02347
AreebMusani <areeb1805a@aptechgdn.net>
2023-04-27 23:54:51 +0500

    Creating About Page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+    return(
+        <div className="">
+            <div className="bg-slate-700 h-64 bg-contain bg-fixed rounded-b-[40%] p-10">
+                <h1 className="text-white text-center font-bold text-5xl mt-10 leading-tight">About Us</h1>
+            </div>
+        </div>
+    )
+}
+
+export default About;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,9 +8,9 @@ const Header = () => {
     <div className="flex justify-between px-10 items-center bg-slate-700">
         <Image className="w-auto" src={"/images/Logo.webp"} width={150} height={150} alt={"web Logo"} />
         <ul className="sm:flex hidden text-white items-center space-x-4 md:space-x-10">
-            <li className="text-lg hover:scale-125 transition-all duration-300"><Link className="" href="#">Home</Link></li>
+            <li className="text-lg hover:scale-125 transition-all duration-300"><Link className="" href="/">Home</Link></li>
             <li className="text-lg hover:scale-125 transition-all duration-300"><Link className="" href="#">Explore</Link></li>
-            <li className="text-lg hover:scale-125 transition-all duration-300"><Link className="" href="#">About</Link></li>
+            <li className="text-lg hover:scale-125 transition-all duration-300"><Link className="" href="/about">About</Link></li>
             <li className="text-lg hover:scale-125 transition-all duration-300"><Link className="" href="#">Contact</Link></li>
             <button className="text-lg font-bold px-6 py-2 rounded-2xl bg-blue-600 hover:bg-blue-700">Apply</button>
         </ul>
