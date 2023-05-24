d343763b108ebc15f21d862a6582ec1f1ff5bc4c
AreebMusani <areeb1805a@aptechgdn.net>
2023-05-25 00:53:15 +0500

    About Page Update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,17 +13,16 @@ const About = () => {
       <div className="flex m-10">
         {/* <div className="w-full h-10"> */}
         <img
-          width={430}
-          height={450}
+          className="w-[450px] h-[600px]"
           src="/images/arifalvi.webp"
           alt="President Alvi"
         />
         {/* </div> */}
-        <div className="bg-gray-400 p-10 my-10 -mx-10">
-          <h1 className="border-l-blue-600 border-l-8 pl-4 font-bold font-serif text-3xl md:text-6xl line leading-tight">
+        <div className="bg-slate-700 text-white p-10 my-10 -ml-10">
+          <h1 className="border-l-blue-600 border-l-8 pl-4 font-bold font-serif text-3xl md:text-4xl line leading-tight">
             Dr. Arif Alvi
           </h1>
-          <p className="my-5 text-justify text-lg">
+          <p className="mt-5 text-justify text-base">
             Dr. Arif Alvi was sworn in as the 13th President of Islamic Republic of Pakistan on 9th September 2018.
             <br /><br />
             Dr. Arif Alvi was born in 1949 and completed his early education in Karachi. He did his Bachelor of Dental Surgery (BDS) from De’ Montmorency College of Dentistry, Lahore where he was declared the “Best Graduate”. He completed his Masters of Science in the field of Prosthodontics from University of Michigan (1975) and in Orthodontics from University of Pacific, San Francisco (1982).
